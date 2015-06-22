8214995f973fdc3ed06781155a9bafaa483ea9d8
Ella <ellapapanek@yahoo.com>
2015-06-22 13:17:42 +0000

    stage 1

---
+++ my_project.rb
@@ -0,0 +1,24 @@
+puts "DISCLAIMER: We, here at Fear Death, Near Death, We're Death (FDNDWD) are not to be held liable for any failure to follow our provided procedures or thus unsuccessful or uncompleted murder plots. Proceed with caution, but also cherish your plot.  We take pride in our procedures and take our business highly seriously.  We guarantee that we WILL provide you with the proper murder plan.  We maintain a 100% satisfaction rate.  After all it IS a life or death situation.  FDNDWD is not responsible for any murders completed because of, with help from, or in affiliation at all with our company.  Cherish your time alive, for it is the only time you may kill.  Violence is always the answer.  Consumere, dormire, necare.  In death we trust."
+
+puts "How many people to you plan to kill?"
+amount_to_kill = gets.chomp
+time_of_day = "we don't know yet"
+if amount_to_kill.to_i >= 50
+  time_of_day = "bright, sunny afternoon, when crowds bustle through the streets and birds chirp happily in the air"
+ if amount_to_kill.to_i >=20
+   time_of_day = "rainy weekend afternoon, when there is nothing much to do and people flock to the shopping malls and stores"
+   if amount_to_kill.to_i >=6
+     time_of_day = "busy, late friday night, when parties bloom all around, and the sun has sunken below the horizon, no longer sending her golden rays across the face of the earth.  However, crowds must continue to bubble and bustle for complete success."
+  #rural-bushes/urban- transportation
+     if amount_to_kill.to_i >=2
+       time_of_day = "a dark, stormy night when thunder booms, lightning flashes, and power outages roam the terrain, stifling the screams from your victims." 
+       if amount_to_kill.to_i ==1
+         time_of_day = "one shadowy dawn when the sun and moon alike stare down as the only witnesses to your vicious attack.  As the crisp morning air settles and fresh dew adorning plants glistens, those around you slumber peacefully, unawares. The slight fog will mask your murder from the as you disappear into the new day."
+ # reasoning/motive
+ # relation
+ # urgency
+
+  #weapon, place, time
+  
+  
+  "One #{time_of_day}, this marks your day.  This plan is designed to enable you to successfully complete the murder within the next urgency_amount. emerge from your home_apartment, wearing a clothes_
